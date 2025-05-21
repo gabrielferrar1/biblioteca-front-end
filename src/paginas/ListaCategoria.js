231e5239d1ff28432e947e5f0ae1085a787af032
Gabriel Ferrari de Oliveira <gbrlferrari@outlook.com>
2025-05-20 22:08:46 -0300

    Desenvolvimento em aula dia 20/05/2025

--- src/paginas/ListaCategoria.js
+++ src/paginas/ListaCategoria.js
@@ -1,12 +1,27 @@
 import TituloLista from "../componentes/TituloLista";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 export default function ListaCategoria() {
+  //Declarando uma variavel de estado (useState) para armazenar os dados
+  const [dados, setDados] = useState([]);
+
+  const listar = async () => {
+    let { data } = await axios.get(`http://localhost:4000/categoria`);
+    console.log(data);
+    setDados(data);
+  };
+
+  useEffect(() => {
+    listar();
+  }, []);
+
   return (
     <>
       <TituloLista
         titulo="Categorias"
         descricao="Gerencie aqui as categorias dos livros da biblioteca"
-        rota="/categoria"
+        rota="/cadastrocategoria"
       />
 
       <div className="row">
@@ -14,30 +29,26 @@ export default function ListaCategoria() {
           <table className="table">
             <thead>
               <tr>
-                <th scope="col">#</th>
-                <th scope="col">First</th>
-                <th scope="col">Last</th>
-                <th scope="col">Handle</th>
+                <th scope="col"></th>
+                <th scope="col">Código</th>
+                <th scope="col">Categoria</th>
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <th scope="row">1</th>
-                <td>Mark</td>
-                <td>Otto</td>
-                <td>@mdo</td>
-              </tr>
-              <tr>
-                <th scope="row">2</th>
-                <td>Jacob</td>
-                <td>Thornton</td>
-                <td>@fat</td>
-              </tr>
-              <tr>
-                <th scope="row">3</th>
-                <td colSpan={2}>Larry the Bird</td>
-                <td>@twitter</td>
-              </tr>
+              {dados.map((d, i) => (
+                <tr>
+                  <td>
+                    <a
+                      className="btn btn-primary"
+                      href={`/cadastrocategoria/${d.id_categoria}`}
+                    >
+                      Alterar
+                    </a>
+                  </td>
+                  <td>{d.id_categoria}</td>
+                  <td>{d.nome_categoria}</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </div>
